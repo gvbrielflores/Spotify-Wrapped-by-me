83bfc31eae7cbd02d441a4a075f956f7444ed349
gvbrielflores <gabrielf.lovo@gmail.com>
2024-06-05 16:45:00 -0500

    started making the login page component, deleted the default vercel/next shit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import HomeLoginPage from "@/components/login";
+import Image from "next/image";
+
+export default function Home() {
+
+  return (
+    <HomeLoginPage/>
+  );
+}
---
+++ src/components/login.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+
+const HomeLoginPage = () => {
+
+    return (
+        <main className="min-h-screen">
+            <div className = "flex flex-col">
+                <h1>Welcome to your monthly Wrapped ;)</h1>
+                <a>by gabriel floreslovo (gabi)</a>
+            </div>
+        </main>
+    )
+}
+
+
+export default HomeLoginPage;
