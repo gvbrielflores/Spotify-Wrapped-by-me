92bb8fb12b63b6fc8040fdf65521c463b21dad62
gvbrielflores <gabrielf.lovo@gmail.com>
2024-06-19 15:44:07 -0500

    beginnings: skeletal color theming, present icon clicks to reveal login (still theoretical). made wrapped page dir with page.tsx, has nothing in it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import HomeLoginPage from "@/components/login";
 import Image from "next/image";
+import { Helmet } from 'react-helmet';
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
 export default function Home() {
 
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,12 +1,28 @@
+'use client';
+
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 
 const HomeLoginPage = () => {
 
-    return (
-        <main className="min-h-screen">
-            <div className = "flex flex-col">
+    const [presentClicked, setPresentClicked] = useState(false);
+
+    return(
+        <main className="flex flex-col min-h-screen justify-between">
+            <div className='flex flex-row justify-center'>
                 <h1>Welcome to your monthly Wrapped ;)</h1>
+            </div>
+            {!presentClicked && <div className='flex flex-row justify-center'>
+                <button onClick={() => setPresentClicked(true)}> {/*have to add ()=> because onClick expects a function*/}
+                    <Image src={'/pixel_gift_box_NOTPAIDSTOCKIMAGE.png'} alt={'wrapped_NOT_PAID'} width={200} height={200}></Image>
+                </button>
+            </div>}
+            {presentClicked && <div className='flex justify-center'>
+                <h1>This is gonna be a login thing</h1>
+            </div>}
+            <div className='flex flex-row justify-center'>
                 <a>by gabriel floreslovo (gabi)</a>
             </div>
         </main>
