c407cce12daa98804e79fda6e05093cd00e84f62
gvbrielflores <gabrielf.lovo@gmail.com>
2024-07-30 13:37:32 -0500

    Deleted useles imports from wrapped_page/page.tsx

--- src/app/wrapped_page/page.tsx
+++ src/app/wrapped_page/page.tsx
@@ -1,22 +1,3 @@
-import GetTopStatsMonth from "@/components/get-top-stats-month";
-import TestButton from "@/components/test";
-import { Button } from "@/components/ui/button"
-import { useState } from "react";
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-  } from "@/components/ui/carousel"
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-import { Value } from "@radix-ui/react-select";
 import WrappedPage from "@/components/wrapped-page";
 
 export default function Page() {
